021097cb915da80c9ea86738d2654d2f65ebbb26
Axucs <Axucs@list.ru>
2016-05-09 21:47:57 +0300

    ку

--- Ugly world/CAttrib.h
+++ Ugly world/CAttrib.h
@@ -3,30 +3,46 @@
 //----------------------------------------------------------
 
 #pragma once
-
-class CAttrib_P {
-private: 
+class AttribBase {
+protected:
 	int value;
 public:
-	CAttrib_P(int X) {
+};
+class CAttrib_P : public AttribBase {
+private: 
+	Attrib_P type;
+public:
+	CAttrib_P(Attrib_P T, int X) {
 		value = X;
+		type = T;
 	}
 	int valueChange(int Change) {
 		int min = 15;
-		if (value = empty1) { min = 0;}
-		else if (value)
-
-
-
-
-
+		if (type == empty1) { min = 0;}
+		if (value <= min) { value = min; }
+		if (value >= min) { value = value + Change; }
+		//if (type == Strength, value >= 50) { Attrib_P::Endurance, Attrib_P::Agility; }
+		//if (type == Agility, value >= 50) { Attrib_P::Strength, Attrib_P::Endurance; }
+		//if (type == Endurance, value >= 50) { Attrib_P::Agility, Attrib_P::Strength; }
 	}
 
 
 };
-class CAttrib_PM {
-
+class CAttrib_PM : public AttribBase {
+private:
+	Attrib_PM type;
+public:
+	CAttrib_PM(Attrib_PM T, int X) {
+		value = X;
+		type = T;
+	}
 };
-class CAttrib_M {
-
+class CAttrib_M : public AttribBase {
+private:
+	Attrib_M type;
+public:
+	CAttrib_M(Attrib_M T, int X) {
+		value = X;
+		type = T;
+	}
 };
