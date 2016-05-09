8c3457383214709ffe7dd07f5ca267623d990ca7
wad <wad@inbox.lv>
2016-05-09 23:50:30 +0300

    test xml loading

--- Ugly world/CAttrib.h
+++ Ugly world/CAttrib.h
@@ -3,6 +3,9 @@
 //----------------------------------------------------------
 
 #pragma once
+
+#include "CharEnums.h"
+
 class AttribBase {
 protected:
 	int value;
---
+++ Ugly world/LoadXMLs.cpp
@@ -0,0 +1,31 @@
+﻿//----------------------------------------------------------
+// заголовок тут (c)
+//----------------------------------------------------------
+
+#include <stdio.h>
+#include <tinyxml2.h>
+
+void LoadWorld() {
+	tinyxml2::XMLDocument doc;
+	if (doc.LoadFile("world.xml") != tinyxml2::XMLError::XML_SUCCESS) {
+		printf("doc.LoadFile(\"world.xml\") error\n");
+		return;
+	}
+	tinyxml2::XMLElement* rootNode = doc.FirstChildElement();
+	if (!rootNode) {
+		printf("\"world.xml\" have not root node\n");
+		return;
+	}
+	tinyxml2::XMLElement* areaNode = rootNode->FirstChildElement("area");
+	if (!areaNode) {
+		printf("\"world.xml\" have not obj node\n");
+		return;
+	}
+	while (areaNode) {
+		//
+		areaNode = areaNode->NextSiblingElement("area");
+	}
+
+}
+
+
