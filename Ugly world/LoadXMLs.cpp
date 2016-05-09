4b29d83908957134417e1d92168e890a575be5aa
wad <wad@inbox.lv>
2016-05-09 23:56:49 +0300

    fineing and loading characters

--- Ugly world/LoadXMLs.cpp
+++ Ugly world/LoadXMLs.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <tinyxml2.h>
 
+//----------------------------------------------------------
 void LoadWorld() {
 	tinyxml2::XMLDocument doc;
 	if (doc.LoadFile("world.xml") != tinyxml2::XMLError::XML_SUCCESS) {
@@ -25,7 +26,29 @@ void LoadWorld() {
 		//
 		areaNode = areaNode->NextSiblingElement("area");
 	}
-
 }
+//----------------------------------------------------------
+void LoadCharacters() {
+	tinyxml2::XMLDocument doc;
+	if (doc.LoadFile("characters.xml") != tinyxml2::XMLError::XML_SUCCESS) {
+		printf("doc.LoadFile(\"character.xml\") error\n");
+		return;
+	}
+	tinyxml2::XMLElement* rootNode = doc.FirstChildElement();
+	if (!rootNode) {
+		printf("\"character.xml\" have not root node\n");
+		return;
+	}
+	tinyxml2::XMLElement* charNode = rootNode->FirstChildElement("character");
+	if (!charNode) {
+		printf("\"character.xml\" have not obj node\n");
+		return;
+	}
+	while (charNode) {
+		//
+		charNode = charNode->NextSiblingElement("character");
+	}
+}
+//----------------------------------------------------------
 
 
